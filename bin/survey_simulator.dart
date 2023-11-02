060b955ee4206d2bff0580c07d2401813c68e2b4
eliasyishak <42216813+eliasyishak@users.noreply.github.com>
2023-11-02 17:05:05 -0400

    Refactor to use `bin/` entrypoint

--- bin/survey_simulator.dart
+++ bin/survey_simulator.dart
@@ -1,5 +1,27 @@
 import 'package:survey_simulator/survey_simulator.dart';
+import 'package:unified_analytics/unified_analytics.dart';
 
-void main(List<String> arguments) {
-  run();
+void functionToRun(FakeAnalytics analyticsInstance) {
+  for (var i = 0; i < 50; i++) {
+    analyticsInstance.send(Event.analyticsCollectionEnabled(status: false));
+  }
+}
+
+void main(List<String> arguments) async {
+  final remoteContent = await fetchContents();
+
+  final simulator = Simulator(
+    label: 'Running on flutter beta channel',
+    surveyId: 'd27d24da-f789-45e3-83af-7ebaeade75c1',
+    simulationDateTime: DateTime.now(),
+    tool: DashTool.dartTool,
+    flutterChannel: 'beta',
+    flutterVersion: 'flutterVersion',
+    dartVersion: 'dartVersion',
+    sendFunction: functionToRun,
+    iterations: 100,
+    remoteContent: remoteContent,
+  );
+
+  print(await simulator.run());
 }
